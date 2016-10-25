46ca4d444959b889b17c8a5628c2acaee0cbab00
Sebastian Abondano <sabondano1@gmail.com>
2016-10-24 21:45:45 -0400

    Add coherence to deps and application in mix.exs

--- apps/merchant_bot_web/mix.exs
+++ apps/merchant_bot_web/mix.exs
@@ -23,7 +23,7 @@ defmodule MerchantBotWeb.Mixfile do
   def application do
     [mod: {MerchantBotWeb, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :coherence]]
   end
 
   # Specifies which paths to compile per environment.
@@ -41,7 +41,8 @@ defmodule MerchantBotWeb.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:coherence, "~> 0.3"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
